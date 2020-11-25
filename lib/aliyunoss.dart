6cfe526f1c8d6dde0467dfaa9cd996b1d50cf196
rm9233 <700985@qq.com>
2020-11-25 10:50:34 +0800

    Update aliyunoss.dart
    拼接字符串的问题

--- lib/aliyunoss.dart
+++ lib/aliyunoss.dart
@@ -30,11 +30,11 @@ class Aliyunoss {
   }
 
   static Future<String> init(String accessKeyId,String accessKeySecret,String stsToken,String bucket,String region) async{
-    region += ".aliyuncs.com";
     final String result = await _channel.invokeMethod('init',{"region":region,"accessKeyId":accessKeyId,"accessKeySecret":accessKeySecret,"stsToken":stsToken,"bucket":bucket});
   }
 
   static Future<String> upload(String path,String bucket,String region,String objectKey) async{
+    region += ".aliyuncs.com";
     final String result = await _channel.invokeMethod('upload',{"path":path,"bucket":bucket,"objectKey":objectKey,"region":region});
   }
 }
